7331c62682f8ed70f156dfa5f5909430ea18989e
Jasper Palfree <well.caffeinated@gmail.com>
2024-12-09 17:37:15 -0700

    Update bloch-sphere.ts

--- src/bloch-sphere.ts
+++ src/bloch-sphere.ts
@@ -13,12 +13,12 @@ export class BlochSphere {
     this.initScene()
   }
 
-  initRenderer() {
+  private initRenderer() {
     this.renderer = new THREE.WebGLRenderer()
     this.renderer.setSize(200, 200)
   }
 
-  initScene() {
+  private initScene() {
     this.scene = new THREE.Scene()
     this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10)
     this.camera.position.z = 1
@@ -27,13 +27,23 @@ export class BlochSphere {
 
     this.sphere = new THREE.Mesh(
       new THREE.SphereGeometry(0.5, 32, 32),
-      new THREE.MeshStandardMaterial({ color: 0x00ff00 })
+      new THREE.MeshNormalMaterial({ wireframe: true })
     )
     this.scene.add(this.sphere)
 
+    // inner sphere
+    const innerSphere = new THREE.Mesh(
+      new THREE.SphereGeometry(0.495, 32, 32),
+      new THREE.MeshBasicMaterial({ color: 0x000000 })
+    )
+    this.scene.add(innerSphere)
+
     const light = new THREE.DirectionalLight(0xffffff, 1)
-    light.position.set(1, 1, 1)
+    light.position.set(1, 0, 1)
     this.scene.add(light)
+
+    const ambientLight = new THREE.AmbientLight(0x404040)
+    this.scene.add(ambientLight)
 
   }
 
@@ -70,7 +80,7 @@ export class BlochSphere {
   }
 
   dispose(){
+    this.stop()
     this.renderer.dispose()
   }
-
 }
